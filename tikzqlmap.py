111da5eaae1939e4ffe2bb09df4c0135c5059273
Jay Turla <shipcodez@gmail.com>
2020-08-11 13:26:53 +0800

    Create tikzqlmap.py

---
+++ tikzqlmap.py
@@ -0,0 +1,36 @@
+tikz = '''
+                                     /~\                            
+                                    |oo )                           
+                                    _\=/_                           
+                    ___            /  _  \                          
+                   /() \          //|/.\|\\                         
+                 _|_____|_        \\ \_/ ||                        
+                | | === | |        \|\ /|||                        
+                |_|  O  |_|        # _ _/#                        
+                 ||  O  ||          | | |                          
+                 ||__*__||          | | |                          
+                |~ \___/ ~|         []|[]                          
+                /=\ /=\ /=\         | | |                          
+________________[_]_[_]_[_]________/_]_[_\_________________________
+ _   _ _              _                       
+| | (_) |            | |                      
+| |_ _| | __ ______ _| |_ __ ___   __ _ _ __  
+| __| | |/ /|_  / _` | | '_ ` _ \ / _` | '_ \ 
+| |_| |   <  / / (_| | | | | | | | (_| | |_) |
+ \__|_|_|\_\/___\__, |_|_| |_| |_|\__,_| .__/ 
+                   | |                 | |    
+                   |_|                 |_|  
+-= automatic pwet grabber exploitation and fingerprinting tool =-
+'''
+
+arguments = '''
+-h 		show help
+-wafnuke 	try to bypass web application firewall 
+--grab-pwet     downloads ass pics 
+-u              URL of the website
+-finger         fingerprints the website
+--crawl         crawls the website recursively based on the hyperlinks
+'''
+
+print tikz
+print arguments
