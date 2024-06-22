e42d4765cd0c8b937a70d38543a3f043889558d6
Jay Turla <shipcodez@gmail.com>
2024-06-22 22:09:36 +0800

    Update tikzqlmap.py

--- tikzqlmap.py
+++ tikzqlmap.py
@@ -1,36 +1,62 @@
+import argparse
+
 tikz = '''
-                                     /~\                            
+                                     /~\\                            
                                     |oo )                           
-                                    _\=/_                           
-                    ___            /  _  \                          
-                   /() \          //|/.\|\\                         
-                 _|_____|_        \\ \_/ ||                        
-                | | === | |        \|\ /|||                        
+                                    _\\=/_                           
+                    ___            /  _  \\                          
+                   /() \\          //|/.\|\\\\                         
+                 _|_____|_        \\\\ \\_/ ||                        
+                | | === | |        \\\\|\\ /|||                        
                 |_|  O  |_|        # _ _/#                        
                  ||  O  ||          | | |                          
                  ||__*__||          | | |                          
-                |~ \___/ ~|         []|[]                          
-                /=\ /=\ /=\         | | |                          
-________________[_]_[_]_[_]________/_]_[_\_________________________
+                |~ \\___/ ~|         []|[]                          
+                /=\\ /=\\ /=\\         | | |                          
+________________[_]_[_]_[_]________/_]_[_\\_________________________
  _   _ _              _                       
 | | (_) |            | |                      
 | |_ _| | __ ______ _| |_ __ ___   __ _ _ __  
-| __| | |/ /|_  / _` | | '_ ` _ \ / _` | '_ \ 
+| __| | |/ /|_  / _` | | '_ ` _ \\ / _` | '_ \\ 
 | |_| |   <  / / (_| | | | | | | | (_| | |_) |
- \__|_|_|\_\/___\__, |_|_| |_| |_|\__,_| .__/ 
+ \\__|_|_|\\_\\\/___\\__, |_|_| |_| |_|\\__,_| .__/ 
                    | |                 | |    
                    |_|                 |_|  
 -= automatic pwet grabber exploitation and fingerprinting tool =-
 '''
 
-arguments = '''
--h 		show help
--wafnuke 	try to bypass web application firewall 
---grab-pwet     downloads ass pics 
--u              URL of the website
--finger         fingerprints the website
---crawl         crawls the website recursively based on the hyperlinks
-'''
+def main():
+    parser = argparse.ArgumentParser(description='Automatic PWET grabber exploitation and fingerprinting tool')
+    
+    parser.add_argument('-w', '--wafnuke', action='store_true', help='try to bypass web application firewall')
+    parser.add_argument('--grab-pwet', action='store_true', help='downloads ass pics')
+    parser.add_argument('-u', '--url', type=str, help='URL of the website')
+    parser.add_argument('-f', '--finger', action='store_true', help='fingerprints the website')
+    parser.add_argument('--crawl', action='store_true', help='crawls the website recursively based on the hyperlinks')
+    
+    args = parser.parse_args()
+    
+    print(tikz)
+    
+    if args.wafnuke:
+        print("Attempting to bypass web application firewall...")
+        # Add your wafnuke code here
+    
+    if args.grab_pwet:
+        print("Downloading ass pics...")
+        # Add your grab-pwet code here
+    
+    if args.url:
+        print(f"Target URL: {args.url}")
+        # Add your code to handle the URL here
+    
+    if args.finger:
+        print("Fingerprinting the website...")
+        # Add your fingerprinting code here
+    
+    if args.crawl:
+        print("Crawling the website recursively based on the hyperlinks...")
+        # Add your crawling code here
 
-print tikz
-print arguments
+if __name__ == "__main__":
+    main()
